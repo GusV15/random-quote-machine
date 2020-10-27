09cd836cb0162ee3e3901b4ac1eb90672737c461
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2020-10-27 01:51:44 -0300

    Updates

---
+++ precache-manifest.01d6864489682875c0ed9de109db836d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b720be71b812f0adb28fd2656ea32d7e",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "6da0049258707b3a54ca",
+    "url": "/random-quote-machine/static/css/main.9e40ca56.chunk.css"
+  },
+  {
+    "revision": "0b2ec59c809acf3c7286",
+    "url": "/random-quote-machine/static/js/2.c268b4fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.c268b4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da0049258707b3a54ca",
+    "url": "/random-quote-machine/static/js/main.e2bfc18f.chunk.js"
+  },
+  {
+    "revision": "2cec414998c69e0a692f",
+    "url": "/random-quote-machine/static/js/runtime-main.5e7c1c70.js"
+  }
+]);
